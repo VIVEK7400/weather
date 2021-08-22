20a989fa66707242282684a7cf5bd65f14450c2c
VIVEK7400 <47096934+VIVEK7400@users.noreply.github.com>
2021-08-22 18:29:13 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,14 @@
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyAI5WSLAsbGm4pne0pEiLip0gUnIPxzUWA",
+    authDomain: "login-2b2b2.firebaseapp.com",
+    projectId: "login-2b2b2",
+    storageBucket: "login-2b2b2.appspot.com",
+    messagingSenderId: "885476600538",
+    appId: "1:885476600538:web:a9c13a87856f1805ed93e3",
+    measurementId: "G-5W8HMFWYVN"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
